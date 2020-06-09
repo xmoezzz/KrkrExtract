1a8d2c66694578501886e9b01b56c5b665c799e3
fengberd <admin@berd.moe>
2020-06-09 08:46:58 +0800

    Fix the encoding conversion issues

--- KrkrExtract/KrkrExtract/Program.cs
+++ KrkrExtract/KrkrExtract/Program.cs
@@ -47,8 +47,8 @@ namespace KrkrExtract
                     return;
                 }
 
-                IntPtr ldrLoadDll = NTAPI.GetProcAddress(NTAPI.GetModuleHandle("kernel32.dll"), "LoadLibraryA");
-                if (ldrLoadDll == IntPtr.Zero)
+                IntPtr loadLibrary = NTAPI.GetProcAddress(NTAPI.GetModuleHandle("kernel32.dll"), "LoadLibraryW");
+                if (loadLibrary == IntPtr.Zero)
                 {
                     MessageBox.Show("GetProcAddress or GetModuleHandle failed: " + Marshal.GetLastWin32Error(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     return;
@@ -61,8 +61,10 @@ namespace KrkrExtract
                     return;
                 }
 
-                byte[] tmp = new byte[dll.Length + 1];
-                Array.Copy(Encoding.UTF8.GetBytes(dll), tmp, dll.Length);
+                // Write DLL Path
+                byte[] tmp = Encoding.GetEncoding("UTF-16").GetBytes(dll);
+                Array.Resize(ref tmp, tmp.Length + 1);
+                tmp[tmp.Length - 1] = 0x00;
 
                 if (!NTAPI.WriteProcessMemory(process.hProcess, buffer, tmp, tmp.Length, out int written) || written != tmp.Length)
                 {
@@ -73,20 +75,20 @@ namespace KrkrExtract
                 long returnAddress = thread.Eip;
                 thread.Eip = (uint)(((uint)buffer + tmp.Length) / 16 + 3) * 16;
 
-                // Write shell code
+                // Construct shell code
                 var shellcode = new byte[]
                 {
                 0x50, // push eax
                 0xb8, 0x00, 0x00, 0x00, 0x00, // mov eax, <Buffer>
                 0x50, // push eax
-                0xb8, 0x00, 0x00, 0x00, 0x00, // mov eax, <LoadLibraryA>
+                0xb8, 0x00, 0x00, 0x00, 0x00, // mov eax, <LoadLibraryW>
                 0xff, 0xd0, // call eax
                 0x58, // pop eax
                 0xe9, 0x00, 0x00, 0x00, 0x00 // jmp <returnAddress>
                 };
 
                 Array.Copy(BitConverter.GetBytes((int)buffer), 0, shellcode, 2, 4);
-                Array.Copy(BitConverter.GetBytes((int)ldrLoadDll), 0, shellcode, 8, 4);
+                Array.Copy(BitConverter.GetBytes((int)loadLibrary), 0, shellcode, 8, 4);
                 Array.Copy(BitConverter.GetBytes(returnAddress - thread.Eip - shellcode.Length), 0, shellcode, shellcode.Length - 4, 4);
 
                 if (!NTAPI.WriteProcessMemory(process.hProcess, (IntPtr)thread.Eip, shellcode, shellcode.Length, out written) || written != shellcode.Length)
